047b94634b340f9389fb23027f0500b367d3c87f
Kobe Ferran <emailme.kobeferran@gmail.com>
2022-02-24 03:15:58 +0800

    responsive nav and cover image

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,31 +1,54 @@
 <template>
-    <div class="container">
-        <div id="cover" class="cover">                                                   
-
-            <h1 id="name">KOBE FERRAN</h1>
-            <h3 id="title">FULL STACK DEVELOPER</h3>
-            <h5>Specializing in Laravel and Vue.js</h5>
-            
-        </div>
-
-        <div id="projects" class="projects">
-
-            <h1 class="feature-title">My Projects</h1>
-
-            <div class="project-card">
-                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere similique excepturi magnam officiis, dicta quis?</h3>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora similique eius omnis, porro odit tempore dignissimos nulla qui tenetur accusantium illo accusamus, nesciunt fuga consectetur facilis voluptatum dicta id!</p>
-            </div>
-            <div class="project-card">
-                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere similique excepturi magnam officiis, dicta quis?</h3>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora similique eius omnis, porro odit tempore dignissimos nulla qui tenetur accusantium illo accusamus, nesciunt fuga consectetur facilis voluptatum dicta id!</p>
-            </div>
-        </div>
+  <div class="container">
+    <div id="cover" class="cover">
+      <div class="cover-banner">
+        <h1 id="name">KOBE FERRAN</h1>
+        <h3 id="title">FULL STACK DEVELOPER</h3>
+        <h5>Specializing in Laravel and Vue.js</h5>        
+      </div>
+      <Button class="btn btn-primary"
+        >Check out My Projects <i class="fa-solid fa-angle-right"></i
+      ></Button>
     </div>
+
+    <div id="projects" class="projects">
+      <h1 class="feature-title">My Projects</h1>
+
+      <div class="project-card">
+        <h3>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+          similique excepturi magnam officiis, dicta quis?
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora
+          similique eius omnis, porro odit tempore dignissimos nulla qui tenetur
+          accusantium illo accusamus, nesciunt fuga consectetur facilis
+          voluptatum dicta id!
+        </p>
+      </div>
+      <div class="project-card">
+        <h3>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
+          similique excepturi magnam officiis, dicta quis?
+        </h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora
+          similique eius omnis, porro odit tempore dignissimos nulla qui tenetur
+          accusantium illo accusamus, nesciunt fuga consectetur facilis
+          voluptatum dicta id!
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Button from "./Button";
+
 export default {
-    name: 'Content',    
-}
+  name: "Content",
+  components: {
+    Button,
+  },
+};
 </script>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,9 @@
 <template>
-  <nav class="navbar" :class="{ 'nav-transparent' : scrollPosition > 100}">
+  <nav class="navbar" :class="{ 'nav-transparent': scrollPosition > 100 }">
     <ul>
+      <a href="" class="logo">
+        <img src="@/assets/img/logo/logo.png" alt="">
+      </a>      
       <li>
         <a href="">
           <span>Home</span>
@@ -16,17 +19,27 @@
           <span>Contact</span>
         </a>
       </li>
+
+      <Button class="btn-primary" id="show-modal" @click="showModal = true">
+        <i class="fa-solid fa-bars" style="font-size: 2.5rem;"></i>
+      </Button>
     </ul>
   </nav>
 </template>
 
 <script>
+import Button from "./Button";
+
 export default {
   name: "Navigation",
   data() {
-      return {
-          scrollPosition: null,
-      }
+    return {
+      scrollPosition: null,
+      showModal: false
+    };
+  },
+  components: {
+    Button,
   },
   methods: {
     updateScroll() {
@@ -36,12 +49,11 @@ export default {
   mounted() {
     window.addEventListener("scroll", this.updateScroll);
   },
-
 };
 </script>
 
 <style scoped>
-   .change_color {
-       background-color:red
-   }
+.change_color {
+  background-color: red;
+}
 </style>
