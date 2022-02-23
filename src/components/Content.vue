a6b6fa2659779829bb9180cf879764d6ead8bbb1
Kobe Ferran <emailme.kobeferran@gmail.com>
2022-02-23 22:55:45 +0800

    first commit

---
+++ src/components/Content.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div id="cover" class="cover">                                                   
+
+            <h1 id="name">KOBE FERRAN</h1>
+            <h3 id="title">FULL STACK DEVELOPER</h3>
+            <h5>Specializing in Laravel and Vue.js</h5>
+            
+        </div>
+
+        <div id="projects" class="projects">
+
+            <h1 class="feature-title">My Projects</h1>
+
+            <div class="project-card">
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere similique excepturi magnam officiis, dicta quis?</h3>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora similique eius omnis, porro odit tempore dignissimos nulla qui tenetur accusantium illo accusamus, nesciunt fuga consectetur facilis voluptatum dicta id!</p>
+            </div>
+            <div class="project-card">
+                <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere similique excepturi magnam officiis, dicta quis?</h3>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi tempora similique eius omnis, porro odit tempore dignissimos nulla qui tenetur accusantium illo accusamus, nesciunt fuga consectetur facilis voluptatum dicta id!</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Content',    
+}
+</script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar" :class="{ 'nav-transparent' : scrollPosition > 100}">
+    <ul>
+      <li>
+        <a href="">
+          <span>Home</span>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <span>About</span>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <span>Contact</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data() {
+      return {
+          scrollPosition: null,
+      }
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+  },
+
+};
+</script>
+
+<style scoped>
+   .change_color {
+       background-color:red
+   }
+</style>
